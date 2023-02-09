58438e4d3fd7bc2fc657324eda4f920ac4b51b1b
Lesleis Nagy <lesleisnagy@gmail.com>
2023-02-09 14:53:42 +0000

    Added additional python versions.

--- setup.py
+++ setup.py
@@ -70,6 +70,8 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
     ],
 )
