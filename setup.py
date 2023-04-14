ee8ddaa3b6feecf7e4b668b7962b72becd39714f
Lesleis-Nagy <49793511+Lesleis-Nagy@users.noreply.github.com>
2023-04-14 09:21:17 +0100

    Update setup.py
    Bumped to 0.2.2.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="synth-forc",
-    version="0.2.1",
+    version="0.2.2",
     description="A tool used to visualise synthetic FORC data generated using micromagnetic models.",
     url="https://github.com/Lesleis-Nagy/synth-forc",
     author="L. Nagy, Miguel A. Valdez-Grijalva, W. Williams, A. Muxworthy,  G. Paterson and L. Tauxe",
